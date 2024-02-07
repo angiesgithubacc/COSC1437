593a8ab066a38e851c16d7882508149e23de4914
angiesgithubacc <157391224+angiesgithubacc@users.noreply.github.com>
2024-02-07 13:58:40 +0000

    Name

--- Name.cpp
+++ Name.cpp
@@ -4,13 +4,28 @@ using namespace std;
 
 int main()
 {
-    string testString;
-    testString = "Hwang Hyunjin";
-    cout << testString.length() << endl << endl;
-    cout << testString.find(" ") << endl;
-    cout << testString.substr(1, 4) << endl;
-    cout << testString.substr(6, 10) << endl;
-    cout << testString[0] << endl;
+
+    //Setting Variables
+    string FirstName;
+    string LastName;
+    int Firstl;
+    int Lastl;
+    int FirstIn;
+    int LastIn;
+
+    //asking for first name
+    cout <<  "What is your name?\n";
+    cin >> FirstName;
+    Firstl = FirstName.length()/2;
+    
+    cout << "What is your last name?\n";
+    cin >> LastName;
+    Lastl = LastName.length()/2;
+
+    cout << "So your first name nickname is " + FirstName.substr(0, Firstl) + LastName.substr(0, Lastl) << endl;
+    cout << "And your last name nickname is " + LastName.substr(0, Lastl) + FirstName.substr(0, Firstl) << endl;
+
 
     return 0;
+
 }
