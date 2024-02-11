f3e612689203c1b12a58a2e6898399845393f938
angiesgithubacc <157391224+angiesgithubacc@users.noreply.github.com>
2024-02-11 22:52:39 +0000

    code

--- Mod7/Mod7_4.cpp
+++ Mod7/Mod7_4.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
-using namespace std;
-
-int main()
-
-{
-
+int main() {
+    int i = 0;
     
-return 0;
-
+    while (i <= 10) {
+        std::cout << "Number: " << i << std::endl;
+        std::cout << "Multiplied by 2: " << i * 2 << std::endl;
+        std::cout << "Multiplied by 10: " << i * 10 << std::endl;
+        std::cout << std::endl;
+        
+        i++;
+    }
+    
+    return 0;
 }
