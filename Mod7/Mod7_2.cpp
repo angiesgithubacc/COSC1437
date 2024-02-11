4be97e59bc9d4da5c05890a61633ad958f07f88d
angiesgithubacc <157391224+angiesgithubacc@users.noreply.github.com>
2024-02-11 22:50:21 +0000

    code

--- Mod7/Mod7_2.cpp
+++ Mod7/Mod7_2.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 
-using namespace std;
+int main() {
+    int rating;
+    int totalRatings = 0;
+    int numSongs = 0;
 
-int main()
+    std::cout << "Welcome to the Top Songs Rating Program!" << std::endl;
+    std::cout << "Please rate the top songs of the week from 0 to 4 stars." << std::endl;
+    std::cout << "Enter a negative number to quit." << std::endl;
 
-{
+    while (true) {
+        std::cout << "Enter your rating: ";
+        std::cin >> rating;
 
-    
-return 0;
+        if (rating < 0) {
+            break;
+        }
 
+        if (rating >= 0 && rating <= 4) {
+            totalRatings += rating;
+            numSongs++;
+        } else {
+            std::cout << "Invalid rating. Please enter a number from 0 to 4." << std::endl;
+        }
+    }
+
+    if (numSongs > 0) {
+        double averageRating = static_cast<double>(totalRatings) / numSongs;
+        std::cout << "Thank you for rating the songs!" << std::endl;
+        std::cout << "The average star rating for the songs is: " << averageRating << std::endl;
+    } else {
+        std::cout << "No ratings provided. Exiting the program." << std::endl;
+    }
+
+    return 0;
 }
