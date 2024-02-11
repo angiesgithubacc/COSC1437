0bd351fc6351de3847971feaf4006e35351ba7a4
angiesgithubacc <157391224+angiesgithubacc@users.noreply.github.com>
2024-02-11 22:49:23 +0000

    code

--- Mod7/Mod7_1.cpp
+++ Mod7/Mod7_1.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
-using namespace std;
-
-int main()
-
-{
-
-    
-return 0;
-
+int main() {
+    for (int i = 0; i <= 10; i++) {
+        std::cout << "Number: " << i << std::endl;
+        std::cout << "Multiplied by 2: " << i * 2 << std::endl;
+        std::cout << "Multiplied by 10: " << i * 10 << std::endl;
+        std::cout << std::endl;
+    }
+    return 0;
 }
