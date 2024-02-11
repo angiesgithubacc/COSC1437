3f4b66ebe183daefeae3bc4a58a94002fc328af2
angiesgithubacc <157391224+angiesgithubacc@users.noreply.github.com>
2024-02-11 22:44:13 +0000

    number

--- Mod6/Mod6_2.cpp
+++ Mod6/Mod6_2.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
+int main() {
+    int num1 = -50;
+    int num2 = 53;
+    int num3 = 78;
 
-{
+    int largest = num1;
+    int smallest = num1;
 
+    if (num2 > largest) {
+        largest = num2;
+    }
 
+    if (num3 > largest) {
+        largest = num3;
+    }
 
-    return 0;    
+    if (num2 < smallest) {
+        smallest = num2;
+    }
+
+    if (num3 < smallest) {
+        smallest = num3;
+    }
+
+    cout << "Largest number: " << largest << endl;
+    cout << "Smallest number: " << smallest << endl;
+
+    return 0;
 }
-
