e5619331328f67e3d1828ae46ea59ecc7200ba9a
angiesgithubacc <157391224+angiesgithubacc@users.noreply.github.com>
2024-02-11 22:54:35 +0000

    code

--- Mod7/Mod7_6.cpp
+++ Mod7/Mod7_6.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 
-using namespace std;
+int main() {
+    char input;
+    int leftHandedCount = 0;
+    int rightHandedCount = 0;
 
-int main()
+    while (true) {
+        std::cout << "Enter L for left-handed or R for right-handed (X to exit): ";
+        std::cin >> input;
 
-{
+        if (input == 'L') {
+            leftHandedCount++;
+        } else if (input == 'R') {
+            rightHandedCount++;
+        } else if (input == 'X') {
+            break;
+        }
+    }
 
-    
-return 0;
+    std::cout << "Number of left-handed people: " << leftHandedCount << std::endl;
+    std::cout << "Number of right-handed people: " << rightHandedCount << std::endl;
 
+    return 0;
 }
