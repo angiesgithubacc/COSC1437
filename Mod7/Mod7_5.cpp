8bfcab13ed839a6a464823455188c6da29d6f1ba
angiesgithubacc <157391224+angiesgithubacc@users.noreply.github.com>
2024-02-11 22:53:41 +0000

    code

--- Mod7/Mod7_5.cpp
+++ Mod7/Mod7_5.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
-using namespace std;
-
-int main()
-
-{
-
+int main() {
+    for (int row = 1; row <= 5; row++) {
+        for (int col = 1; col <= 3; col++) {
+            if (row == 1 || row == 3 || row == 5) {
+                std::cout << "*";
+            } else if (col == 1) {
+                std::cout << "*";
+            } else {
+                std::cout << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
     
-return 0;
-
+    return 0;
 }
